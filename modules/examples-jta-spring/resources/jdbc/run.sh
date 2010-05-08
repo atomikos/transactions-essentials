2e7d0bb27a93b0020b5f2f70aa318fa13a1407f5
guy <none@none>
2010-05-08 13:46:33 +0200

    Removed examples and README files because they are ant-specific.

--- modules/examples-jta-spring/resources/jdbc/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd ../../..
-. "./setCP.sh"
-cd -
-java -classpath "$CLASSPATH" -Dcom.atomikos.icatch.file=jta.properties jdbc.Main  
-echo
-
--- modules/examples-jta-spring/resources/jms/sendOrder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ../../..
-. "./setCP.sh"
-cd -
-#java -classpath "$CLASSPATH" jms.util.StartBroker 61616 >& /dev/null  &
-java -classpath "$CLASSPATH" -Dlog4j.logger.org.activemq=ERROR jms.OrderWithdrawal   
-echo
-
