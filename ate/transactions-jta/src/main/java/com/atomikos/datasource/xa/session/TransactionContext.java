64bad43f074e2a33c13af4d8a57d5c2948e37b2a
guypardon <none@none>
2010-05-21 15:45:10 +0200

    added license headers with the maven license plugin

--- ate/transactions-jta/src/main/java/com/atomikos/datasource/xa/session/TransactionContext.java
+++ ate/transactions-jta/src/main/java/com/atomikos/datasource/xa/session/TransactionContext.java
@@ -1,3 +1,28 @@
+/**
+ * Copyright (C) 2000-2010 Atomikos <info@atomikos.com>
+ *
+ * This code ("Atomikos TransactionsEssentials"), by itself,
+ * is being distributed under the
+ * Apache License, Version 2.0 ("License"), a copy of which may be found at
+ * http://www.atomikos.com/licenses/apache-license-2.0.txt .
+ * You may not use this file except in compliance with the License.
+ *
+ * While the License grants certain patent license rights,
+ * those patent license rights only extend to the use of
+ * Atomikos TransactionsEssentials by itself.
+ *
+ * This code (Atomikos TransactionsEssentials) contains certain interfaces
+ * in package (namespace) com.atomikos.icatch
+ * (including com.atomikos.icatch.Participant) which, if implemented, may
+ * infringe one or more patents held by Atomikos.
+ * It should be appreciated that you may NOT implement such interfaces;
+ * licensing to implement these interfaces must be obtained separately from Atomikos.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 package com.atomikos.datasource.xa.session;
 
 import javax.transaction.xa.XAResource;
