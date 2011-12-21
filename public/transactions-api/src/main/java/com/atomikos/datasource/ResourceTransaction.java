6b3b883cdc9be8d4ac09a3f2220d5badc707942d
guypardon <none@none>
2011-12-21 19:46:05 +0100

    cleaned up code

--- public/transactions-api/src/main/java/com/atomikos/datasource/ResourceTransaction.java
+++ public/transactions-api/src/main/java/com/atomikos/datasource/ResourceTransaction.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2000-2010 Atomikos <info@atomikos.com>
+ * Copyright (C) 2000-2011 Atomikos <info@atomikos.com>
  *
  * This code ("Atomikos TransactionsEssentials"), by itself,
  * is being distributed under the
@@ -26,32 +26,27 @@
 package com.atomikos.datasource;
 
 import com.atomikos.icatch.HeuristicMessage;
+
 /**
- *
- *
- *The notion of a local transaction executed on a resource.
- *Serves as a handle  towards the transaction management module.
+ * The notion of a local transaction executed on a resource.
+ * Serves as a handle towards the transaction management module.
  */
  
 public interface ResourceTransaction 
 {
-   
-    
     
     /**
-     *Get identifier for this tx.
-     *Should be unique in system.
      *
-     *@return String The identifier, as determined by resource.
+     * @return String A unique identifier, as determined by resource.
      */
 
     public java.lang.String getTid();
    
 
     /**
-     *Add heuristic resolution information.
-     *@param mesg The heuristic message.
-     *@exception IllegalStateException If no longer active.
+     * Adds heuristic resolution information.
+     * @param mesg The heuristic message.
+     * @exception IllegalStateException If no longer active.
      */
 
     public void addHeuristicMessage(HeuristicMessage mesg)
@@ -59,38 +54,25 @@ public interface ResourceTransaction
     
    
     /**
-     *Get heuristic context info.
      *
-     *@return HeuristicMessage[] An array of messages, or null if none.
+     * @return HeuristicMessage[] An array of messages, or null if none.
      */
 
     public HeuristicMessage[] getHeuristicMessages();
 
 
     /**
-     *Suspend the resourcetransaction, so that underlying resources can
-     *be used for a next (sibling) invocation.
-     *This is also the recommended method for adding the 
-     *resourcetx to the coordinator object, but ONLY if 
-     *the transaction has not been set for rollback.
-     *NOTE: suspend is NOT the same as XAResource's suspension!
-     *The XAResource version is specific to the XA protocol, 
-     *and does not belong in the composite system framework.
-     *As mentioned in the JTA specs., the APPLICATION SERVER is 
-     *responsible for XAsuspension and XAresume ( page 11 of JTA
-     *API, version of May 12, 1999 ).
+     * Suspends the work, so that underlying resources can
+     * be used for a next (sibling) invocation.
      *
-     *@exception IllegalStateException If wrong state.
      */
 
     public void suspend() throws IllegalStateException,ResourceException;
 
     /**
-     *Resume a suspended tx.
+     * Resumes a previously suspended tx.
      *
-     *@exception IllegalStateException If not right state.
      */
-
 
     public void resume() throws IllegalStateException,ResourceException;
        
