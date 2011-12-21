8e5669a0e2c2ec7d6cf17389a1c031fb1c9c7037
guypardon <none@none>
2011-12-21 20:19:21 +0100

    cleaned up code

--- public/transactions-api/src/main/java/com/atomikos/icatch/CompositeTransaction.java
+++ public/transactions-api/src/main/java/com/atomikos/icatch/CompositeTransaction.java
@@ -31,102 +31,88 @@ import java.util.Stack;
 import com.atomikos.finitestates.Stateful;
 
 /**
- *
- *
- *A composite transaction is a nested part of a global
- *composite transaction. Each invocation of a server is 
- *represented by an instance of this type. For transaction
- *proxies (instances that represent non-local transactions),
- *all non-trivial methods are allowed to generate
- *an exception of type UnsupportedOperationException. The only
- *methods that always work for all instances are the simple 
- *getters and test methods.
+ * Represents a nested part of a global
+ * composite transaction. Each invocation of a server is 
+ * represented by an instance of this type. For transaction
+ * proxies (instances that represent non-local transactions),
+ * all non-trivial methods are allowed to generate
+ * an exception of type UnsupportedOperationException. The only
+ * methods that always work for all instances are the simple 
+ * getters and test methods.
  *
  *
  *
  */
 
-//@todo check exceptions: runtime or not?
-//TODO UPDATE SAMPLES AND DOCS TO ADDED METHODS OF CONTROL AND TERMINATOR
 public interface CompositeTransaction 
     extends Stateful
 {
 	
 	/**
-	 * Get the current state.
+	 * Gets the current state.
 	 * @return Object One of the state constants.
 	 * @see TxState
 	 */
 	public Object getState();
 
     /**
-     *Test if this instance is a  root or not.
-     *Note: a root is the top-level transaction; that is: the first transaction
-     *that was created in the distributed transaction hierarchy.
      *
-     *@return True if this is the root.
+     * @return boolean True if this is the root transaction,
+     * i.e. the first transaction created in a (possibly distributed) hierarchy.
      */
 
     public boolean isRoot();
 
     /**
-     *Get the ancestor info.
-     *@return Stack A stack of ancestors, bottom one is root.
+     * @return Stack A stack of ancestors, bottom one is the root.
      */
 
     public Stack getLineage();
 
 
     /**
-     *Getter for tid.
-     *
-     *@return String The tid for the tx.
+     * 
+     * @return String The tid for the tx.
      */
 
     public String getTid();
 
     /**
-     *Test if this instance is an  ancestor of ct.
-     *
-     *@param ct The argument to test for ancestor.
-     *
-     *@return boolean True iff this is an ancestor of ct.
+     * 
+     * @param otherCompositeTransaction
+     * 
+     * @return boolean True if this instance is an ancestor of the supplied transaction.
      */
 
-    public boolean isAncestorOf( CompositeTransaction ct );
+    public boolean isAncestorOf( CompositeTransaction otherCompositeTransaction );
 
     /**
-     *Test if this instance is a descendant of ct.
-     *
-     *@param ct The argument to test for descendant.
-     *
-     *@return boolean True iff this instance is a descendant of ct.
+     * @param otherCompositeTransaction
+     * 
+     * @return boolean True if this instance is a descendant of the other instance.
      */
 
-    public boolean isDescendantOf( CompositeTransaction ct );
+    public boolean isDescendantOf( CompositeTransaction otherCompositeTransaction );
 
 
     /**
-     *Test if this instance is related to ct.
-     *@return True if related. That is: if both share the same root transaction.
+     * @param otherCompositeTransaction
+     *
+     * @return True if related. That is: if both share the same root transaction.
      */
 
-    public boolean isRelatedTransaction ( CompositeTransaction ct );
+    public boolean isRelatedTransaction ( CompositeTransaction otherCompositeTransaction );
 
     
     /**
-     *Test if this intance represents the same transaction as ct.
-     *@return True iff the same.
+     * @param otherCompositeTransaction
+     * @return True if both are the same.
      */
 
-    public boolean isSameTransaction ( CompositeTransaction ct );
+    public boolean isSameTransaction ( CompositeTransaction otherCompositeTransaction );
 
     /**
-     *Get the coordinator for this tx.
-     *
-     *@return CompositeCoordinator The composite coordinator instance.
-     *
-     *@exception SysException On failure.
+     * @return CompositeCoordinator 
      */
     
     public CompositeCoordinator getCompositeCoordinator() 
@@ -134,31 +120,24 @@ public interface CompositeTransaction
     
 
         
-    /**
-     *Add a new participant.
-     *
-     *@param participant The participant to add.
-     *@return RecoveryCoordinator Whom to ask for indoubt timeout resolution.
-     *@exception SysException Unexpected.
-     *@exception IllegalStateException Illegal state.
-     *
+    /** 
+     * @param participant 
+     * 
+     * @return RecoveryCoordinator Whom to ask for indoubt timeout resolution.
      */
 
     public RecoveryCoordinator addParticipant ( Participant participant )
         throws SysException,
 	     java.lang.IllegalStateException;
 	     
-	
-    /**
-     *Add a synchronization callback.
-     *
-     *@param sync The callback object.
-     *@exception IllegalStateException If no tx longer active.
-     *
-     *
-     *@exception SysException Unexptected failure.
-     */
 
+    /**
+     * 
+     * @param sync
+     * @throws IllegalStateException
+     * @throws SysException
+     */
+    
     public void registerSynchronization(Synchronization sync)
         throws
              IllegalStateException,
@@ -167,136 +146,117 @@ public interface CompositeTransaction
 
 
     /**
-     *Register a participant on behalf of a subtransaction, to be 
-     *notified as soon as the locally finished state is reached.
-     *Resources that support lock inheritance can use this feature
-     *to be notified whenever a lock should be inherited.
+     * Resources that support lock inheritance can use this feature
+     * to be notified whenever a lock should be inherited.
      *
-     *@param subtxaware The participant to be notified 
-     *on local termination.
-     *
-     *@exception IllegalStateException If no longer active.
+     * @param subtxaware
+     * @throws SysException
+     * @throws java.lang.IllegalStateException
      */
 
     public void addSubTxAwareParticipant( SubTxAwareParticipant subtxaware )
         throws SysException,
 	     java.lang.IllegalStateException;
 
-    /**
-     *Test if serial tx or not.
-     *Serial mode is an optimized way for lock inheritance:
-     *no locks among related transactions are necessary if all related
-     *transactions are executed serially with respect to each other.
-     *The serial property is set by the root transaction and is 
-     *propagated to all its subtransactions.
-     *
-     *@return boolean True iff all for root is serial. 
-     */
      
+    /**
+     * Serial mode is an optimized way for lock inheritance:
+     * no locks among related transactions are necessary if all related
+     * transactions are executed serially with respect to each other.
+     * The serial property is set by the root transaction and is 
+     * propagated to all its subtransactions.
+     * 
+     * @return 
+     */
     public boolean isSerial();
     
     /**
-     *Get a control object for this tx.
      *
-     *@deprecated As from release 3.0, 
-     *the methods in the TransactionControl
-     *interface have been moved to this one.
-     *@return TransactionControl a control object.
+     * @deprecated As from release 3.0, 
+     * the methods in the TransactionControl
+     * interface have been moved to this one.
      */
      
     public TransactionControl getTransactionControl();
     
     
     /**
-     *Test if the transaction is a locally started transaction or not.
      *
-     *@return boolean True iff started in the local VM.
-     *For imported transactions, this is false.
+     * @return boolean True if started in the local VM.
+     * For imported transactions, this is false.
      */
      
     public boolean isLocal ();
-    
-    
-        
-	/**
-	 *Create a subtx for this transaction.
-     *For activities, the subtransaction will also be 
-     *an activity.
-	 *
-	 *@return CompositeTransaction The subtx.
-	 *@exception IllegalStateException If no longer active.
-	 */
+
 
 	public CompositeTransaction createSubTransaction()
 		throws SysException,
 		 IllegalStateException;
-	     
-	/**
-	 *Set serial mode for root.
-	 *This only works on the root itself, and can not be undone.
-	 *After this, no parallel calls are allowed in any descendant.
-	 *@exception IllegalStateException If  called for non-root tx.
-	 *@exception SysException For unexpected errors.
-	 */
 
+   
+	/**
+	 * Sets the serial mode. 
+	 * This only works on the root itself, and can not be undone.
+	 * After this, no parallel calls are allowed in any descendant.
+	 *
+	 * @throws IllegalStateException
+	 * @throws SysException
+	 */
+	
 	public void setSerial() throws IllegalStateException, SysException;
 
 	     
     
 	/**
-	 *Get the number of subtxs that were locally started for this
-	 *instance.
-	 *@return int The number of locally started subtxs.
-     *@deprecated This should not matter outside the core.
+     * @deprecated This should not matter outside the core.
 	 */
      
 	 public int getLocalSubTxCount();
      
 	/**
-	 *Sets the tag for this transaction. This is returned as a summary of
-	 *the local work in case the transaction was imported from a remote
-	 *client TM.
-	 *
-	 *@param tag The tag to add to the transaction.
+	 * Sets the tag for this transaction. This is returned as a summary of
+	 * the local work in case the transaction was imported from a remote
+	 * client TM.
+	 * 
+	 * @param tag
 	 */
      
 	public void setTag ( HeuristicMessage tag ) ;
 
 
 	/**
-	 *Get the extent for the transaction.
+	 * 
+	 * @return The extent.
 	 */
 
 	 public Extent getExtent();
      
      
 	  /**
-	   *Get the timeout in ms.
-	   *
-	   *@return long The timeout, in ms, of the tx.
+	   * @return long The transaction timeout in millis.
 	   */
        
 	 public long getTimeout();
 	
 	 /**
-	  *Marks the transaction so that the only possible
-	  *termination is rollback. 
+	  * Marks the transaction so that the only possible
+	  * termination is rollback. 
 	  *
 	  */
 	
 	 public void setRollbackOnly();
 	 
 	/**
-	 *Commit the composite transaction.
+	 * Commits the composite transaction.
 	 *
-	 *@exception HeurRollbackException On heuristic rollback.
-	 *@exception HeurMixedException On heuristic mixed outcome.
-	 *@exception SysException For unexpected failures.
-	 *@exception SecurityException If calling thread does not have 
-	 *right to commit.
-	 *@exception HeurHazardException In case of heuristic hazard.
-	 *@exception RollbackException If the transaction was rolled back
-	 *before prepare.
+	 * @exception HeurRollbackException On heuristic rollback.
+	 * @exception HeurMixedException On heuristic mixed outcome.
+	 * @exception SysException For unexpected failures.
+	 * @exception SecurityException If calling thread does not have 
+	 * right to commit.
+	 * @exception HeurHazardException In case of heuristic hazard.
+	 * @exception RollbackException If the transaction was rolled back
+	 * before prepare.
 	 */
 
 	public void commit() 
@@ -309,9 +269,9 @@ public interface CompositeTransaction
 
 
 	/**
-	 *Rollback the current transaction.
-	 *@exception IllegalStateException If prepared or inactive.
-	 *@exception SysException If unexpected error.
+	 * Rollback of the current transaction.
+	 * @exception IllegalStateException If prepared or inactive.
+	 * @exception SysException If unexpected error.
 	 */
 
 	public void rollback()
@@ -336,12 +296,14 @@ public interface CompositeTransaction
      * @param name The name of the property.
      * @return The property, or null if not set.
      */
+    
     public String getProperty ( String name );
     
     /**
      * Gets all properties of this instance.
      * @return The (cloned) properties of this transaction.
      */
+    
     public Properties getProperties();
 
 
