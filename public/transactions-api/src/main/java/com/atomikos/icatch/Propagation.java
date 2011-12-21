59a9ed7fb35ec4d48fe8c327cc946eb3141e9c8d
guypardon <none@none>
2011-12-21 21:01:52 +0100

    cleaned up code

--- public/transactions-api/src/main/java/com/atomikos/icatch/Propagation.java
+++ public/transactions-api/src/main/java/com/atomikos/icatch/Propagation.java
@@ -27,54 +27,34 @@ package com.atomikos.icatch;
 import java.util.Stack;
 
 /**
- *
- *
- *Information to propagate a transaction to a remote server.
- *
- *
- *
+ * Information about the transaction context that can be 
+ * shipped along with remote request, to make the other side
+ * participate in the transaction present for the current thread in this VM.
  */
 
 public interface Propagation extends java.io.Serializable 
 {
     /**
-     *Get the ancestor information as a stack.
+     * Gets the ancestor information as a stack.
      *
-     *@return Stack The ancestor transactions.
+     * @return Stack The ancestor transactions.
      */
 
     public Stack getLineage();
     
     /**
-     *Test if serial mode or not; if serial, no parallelism allowed.
      *
-     *@return boolean True iff serial mode is set.
+     * @return boolean True if serial mode was set.
      */
     
     public boolean isSerial();
     
     
      /**
-      *Get the timeout left for the composite transaction.
       *
-      *@return long The time left before timeout.
+      *@return long The time left before timeout, in millis.
       */
       
     public long getTimeOut();
     
-//    /**
-//     * Tests if the transaction should be an activity or not. 
-//     * Note that this implies that either 
-//     * ALL transactions in a propagation are activities 
-//     * or NONE is. This makes sense because the
-//     * propagation delimits the SCOPE of the distributed
-//     * transaction, and this scope is either longer-lived 
-//     * (activity) or 
-//     * not, but never both. The scope inherently determines the
-//     * moment at which termination is done via two-phase commit.
-//     * 
-//     * @return boolean True iff recoverable while active.
-//     * 
-//     */
-//    public boolean isActivity();
 }
