f6c387d72d313c734ce8062c9022a7468c0bf298
guypardon <none@none>
2011-12-21 20:30:19 +0100

    cleaned up code

--- public/transactions-api/src/main/java/com/atomikos/icatch/ExportingTransactionManager.java
+++ public/transactions-api/src/main/java/com/atomikos/icatch/ExportingTransactionManager.java
@@ -26,48 +26,38 @@
 package com.atomikos.icatch;
 
 /**
- *
- *
- *An interface for a TM that allows outgoing remote calls to be 
- *transactional.
- *
- *<b>
- *WARNING: this interface and its mechanisms are subject to several patents and
- *pending patents held by Atomikos. Regardless the license
- *under which this interface is distributed, third-party use is 
- *NOT allowed without the prior and explicit 
- *written approval of Atomikos.
- *</b>
- *
+ * An interface for a TM that allows outgoing remote calls to be transactional.
  */
  
  public interface ExportingTransactionManager 
  {
     /** 
-     *Get the propagation info of the tx for the calling thread.
-     *Called before doing the remote call.
+     * Gets the propagation info of the transaction for the calling thread.
+     * Should be called before doing the remote call.
      *
-     *@return Propagation The propagation for the current thread.
+     * @return Propagation The propagation for the current thread.
      *
-     *@exception SysException If no tx for current thread.
-     *@exception RollbackException If the current transaction
-     *has already rolled back.
+     * @exception RollbackException If no such transaction exists, e.g. after a prior rollback.
+     * 
+     * @exception SysException 
+     *
      */
-     
     
     public Propagation getPropagation () 
     throws SysException, RollbackException;
     
     /**
-     *Called after call returns successfully:
-     *add the extent of the call to the
-     *current tx.
-     *If a remote call has failed, this method should NOT be called.
+     * Should be called after call returns successfully:
+     * adds the extent of the call to the current transaction.
      *
-     *@param extent The extent of the call.
-     *@exception RollbackException If the current transaction
-     *has already rolled back.
-     *@exception SysException On failure.
+     * If a remote call has failed, this method should NOT be called.
+     *
+     * @param extent The extent of the call.
+     * 
+     * @exception RollbackException If the current transaction
+     * has already rolled back.
+     * 
+     * @exception SysException On failure.
      */
      
      
