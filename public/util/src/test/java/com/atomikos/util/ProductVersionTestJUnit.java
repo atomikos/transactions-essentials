998eaa66e8afebaad8e2ff8a2416738ece22f0d1
pascalleclercq <none@none>
2012-02-28 00:53:23 +0100

    refactor to be conform to the standard naming scheme

--- public/util/src/test/java/com/atomikos/util/ProductVersionTestJUnit.java
+++ public/util/src/test/java/com/atomikos/util/ProductVersionTestJUnit.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class UtilTest  {
+public class ProductVersionTestJUnit  {
 
 	@Test
 	public void checkAtomikosVersion() {
