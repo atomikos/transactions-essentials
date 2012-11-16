cffaa80c088ad130a6004dee5f4d17fa03b24265
pascalleclercq <none@none>
2012-11-16 17:43:03 +0100

    ACTIVE - case 94753: Check for null before adding HeuristicMessage Participant implementations of JDBC/JMS http://fogbugz.atomikos.com/default.asp?94753

--- public/transactions/src/main/java/com/atomikos/icatch/imp/CoordinatorLogImage.java
+++ public/transactions/src/main/java/com/atomikos/icatch/imp/CoordinatorLogImage.java
@@ -46,273 +46,255 @@ import com.atomikos.util.ClassLoadingHelper;
  * A log image for CoordinatorImp instances.
  */
 
-public class CoordinatorLogImage implements ObjectImage,DataSerializable
-{
+public class CoordinatorLogImage implements ObjectImage, DataSerializable {
 
-    // force serial version UID for backward log compatibility
-    static final long serialVersionUID = 3404629869531420208L;
+	// force serial version UID for backward log compatibility
+	static final long serialVersionUID = 3404629869531420208L;
 
-    String root_;
+	String root_;
 
-    TxState state_;
+	TxState state_;
 
-    boolean heuristicCommit_;
-    // what to do on timeout of indoubt: commit or not
+	boolean heuristicCommit_;
+	// what to do on timeout of indoubt: commit or not
 
-    Vector participants_;
-    // all participants known for this coordinator.
+	Vector participants_;
+	// all participants known for this coordinator.
 
-    RecoveryCoordinator coordinator_;
-    // the recovery coordinator; null if root.
+	RecoveryCoordinator coordinator_;
+	// the recovery coordinator; null if root.
 
-    long maxInquiries_;
-    // max no of indoubt inquiries before heuristic
+	long maxInquiries_;
+	// max no of indoubt inquiries before heuristic
 
-    CoordinatorStateHandler stateHandler_;
+	CoordinatorStateHandler stateHandler_;
 
-    boolean activity_;
-    //activity or not
+	boolean activity_;
+	// activity or not
 
-    int localSiblingCount_;
-    //for activity: active state to recover
+	int localSiblingCount_;
+	// for activity: active state to recover
 
-    boolean checkSiblings_;
-    //for activity: active state to recover
+	boolean checkSiblings_;
+	// for activity: active state to recover
 
-    boolean single_threaded_2pc_;
-    //2PC without threads desired?
+	boolean single_threaded_2pc_;
 
-    /**
-     * Required by Externalizable interface.
-     */
+	// 2PC without threads desired?
 
-    public CoordinatorLogImage ()
-    {
+	/**
+	 * Required by Externalizable interface.
+	 */
 
-    }
+	public CoordinatorLogImage() {
 
-    /**
-     * Constructor for non-activities.
-     *
-     * @param root
-     * @param state
-     * @param participants
-     * @param coordinator
-     * @param commit_on_heuristic
-     * @param maxinquiries
-     * @param stateHandler
-     * @param single_threaded_2pc
-     */
-    public CoordinatorLogImage ( String root , TxState state ,
-            Vector participants , RecoveryCoordinator coordinator ,
-            boolean commit_on_heuristic , long maxinquiries ,
-            CoordinatorStateHandler stateHandler , boolean single_threaded_2pc )
-    {
-        root_ = root;
-        state_ = state;
-        participants_ = participants;
-        coordinator_ = coordinator;
-        heuristicCommit_ = commit_on_heuristic;
-        maxInquiries_ = maxinquiries;
-        stateHandler_ = stateHandler;
-        activity_ = false;
-        localSiblingCount_ = 0;
-        checkSiblings_ = false;
-        single_threaded_2pc_ = single_threaded_2pc;
-    }
+	}
 
-    /**
-     * Constructor for activities in active state.
-     *
-     * @param root
-     * @param state
-     * @param participants
-     * @param coordinator
-     * @param commit_on_heuristic
-     * @param maxinquiries
-     * @param stateHandler
-     * @param localSiblingCount
-     * @param checkSiblings
-     * @param single_threaded_2pc
-     */
-    public CoordinatorLogImage ( String root , TxState state ,
-            Vector participants , RecoveryCoordinator coordinator ,
-            boolean commit_on_heuristic , long maxinquiries ,
-            CoordinatorStateHandler stateHandler , int localSiblingCount ,
-            boolean checkSiblings , boolean single_threaded_2pc
+	/**
+	 * Constructor for non-activities.
+	 *
+	 * @param root
+	 * @param state
+	 * @param participants
+	 * @param coordinator
+	 * @param commit_on_heuristic
+	 * @param maxinquiries
+	 * @param stateHandler
+	 * @param single_threaded_2pc
+	 */
+	public CoordinatorLogImage(String root, TxState state, Vector participants, RecoveryCoordinator coordinator, boolean commit_on_heuristic, long maxinquiries, CoordinatorStateHandler stateHandler,
+			boolean single_threaded_2pc) {
+		root_ = root;
+		state_ = state;
+		participants_ = participants;
+		coordinator_ = coordinator;
+		heuristicCommit_ = commit_on_heuristic;
+		maxInquiries_ = maxinquiries;
+		stateHandler_ = stateHandler;
+		activity_ = false;
+		localSiblingCount_ = 0;
+		checkSiblings_ = false;
+		single_threaded_2pc_ = single_threaded_2pc;
+	}
 
-            )
-    {
-        this ( root , state , participants , coordinator , commit_on_heuristic ,
-                maxinquiries , stateHandler , single_threaded_2pc );
-        activity_ = true;
-        localSiblingCount_ = localSiblingCount;
-        checkSiblings_ = checkSiblings;
-    }
+	/**
+	 * Constructor for activities in active state.
+	 *
+	 * @param root
+	 * @param state
+	 * @param participants
+	 * @param coordinator
+	 * @param commit_on_heuristic
+	 * @param maxinquiries
+	 * @param stateHandler
+	 * @param localSiblingCount
+	 * @param checkSiblings
+	 * @param single_threaded_2pc
+	 */
+	public CoordinatorLogImage(String root, TxState state, Vector participants, RecoveryCoordinator coordinator, boolean commit_on_heuristic, long maxinquiries, CoordinatorStateHandler stateHandler,
+			int localSiblingCount, boolean checkSiblings, boolean single_threaded_2pc
 
-    public Object getId ()
-    {
-        return root_;
-    }
+	) {
+		this(root, state, participants, coordinator, commit_on_heuristic, maxinquiries, stateHandler, single_threaded_2pc);
+		activity_ = true;
+		localSiblingCount_ = localSiblingCount;
+		checkSiblings_ = checkSiblings;
+	}
 
-    public void writeExternal ( ObjectOutput out ) throws IOException
-    {
-        // make sure to CLONE all objects, to avoid that
-        // subsequent calls do NOT re-write the objects
-        // (SERIALIZATION!)
-        out.writeObject ( root_ );
-        out.writeObject ( state_ );
-        out.writeObject ( participants_.clone () );
-        if ( coordinator_ == null )
-            out.writeBoolean ( false );
-        else {
-            out.writeBoolean ( true );
-            out.writeObject ( coordinator_ );
-        }
-        out.writeBoolean ( heuristicCommit_ );
-        out.writeLong ( maxInquiries_ );
-        out.writeObject ( stateHandler_.clone () );
-        out.writeBoolean ( activity_ );
-        if ( activity_ ) {
-            out.writeInt ( localSiblingCount_ );
-            out.writeBoolean ( checkSiblings_ );
-        }
-        out.writeBoolean ( single_threaded_2pc_ );
+	public Object getId() {
+		return root_;
+	}
 
-    }
+	public void writeExternal(ObjectOutput out) throws IOException {
+		// make sure to CLONE all objects, to avoid that
+		// subsequent calls do NOT re-write the objects
+		// (SERIALIZATION!)
+		out.writeObject(root_);
+		out.writeObject(state_);
+		out.writeObject(participants_.clone());
+		if (coordinator_ == null)
+			out.writeBoolean(false);
+		else {
+			out.writeBoolean(true);
+			out.writeObject(coordinator_);
+		}
+		out.writeBoolean(heuristicCommit_);
+		out.writeLong(maxInquiries_);
+		out.writeObject(stateHandler_.clone());
+		out.writeBoolean(activity_);
+		if (activity_) {
+			out.writeInt(localSiblingCount_);
+			out.writeBoolean(checkSiblings_);
+		}
+		out.writeBoolean(single_threaded_2pc_);
 
-    public void readExternal ( ObjectInput in ) throws IOException,
-            ClassNotFoundException
-    {
-        try {
-			root_ = (String) in.readObject ();
-			state_ = (TxState) in.readObject ();
-			participants_ = (Vector) in.readObject ();
-			boolean readcoord = in.readBoolean ();
-			if ( readcoord )
-			    coordinator_ = (RecoveryCoordinator) in.readObject ();
-			heuristicCommit_ = in.readBoolean ();
-			maxInquiries_ = in.readLong ();
-			stateHandler_ = (CoordinatorStateHandler) in.readObject ();
+	}
+
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+		try {
+			root_ = (String) in.readObject();
+			state_ = (TxState) in.readObject();
+			participants_ = (Vector) in.readObject();
+			boolean readcoord = in.readBoolean();
+			if (readcoord)
+				coordinator_ = (RecoveryCoordinator) in.readObject();
+			heuristicCommit_ = in.readBoolean();
+			maxInquiries_ = in.readLong();
+			stateHandler_ = (CoordinatorStateHandler) in.readObject();
 			activity_ = in.readBoolean();
-			if ( activity_ ) {
-			    localSiblingCount_ = in.readInt();
-			    checkSiblings_ = in.readBoolean();
+			if (activity_) {
+				localSiblingCount_ = in.readInt();
+				checkSiblings_ = in.readBoolean();
 			}
 			single_threaded_2pc_ = in.readBoolean();
 		} catch (InvalidClassException ex) {
 			// fix for 22174
-			throw (IOException) new IOException ( "Object of class " + ex.classname + " in transaction log" +
-					" not compatible with version found in classpath" ).initCause(ex);
+			throw (IOException) new IOException("Object of class " + ex.classname + " in transaction log" + " not compatible with version found in classpath").initCause(ex);
 		}
-    }
+	}
 
-    public Recoverable restore ()
-    {
-        CoordinatorImp coord = new CoordinatorImp ();
-        coord.restore ( this );
-        return coord;
-    }
+	public Recoverable restore() {
+		CoordinatorImp coord = new CoordinatorImp();
+		coord.restore(this);
+		return coord;
+	}
 
 	public void writeData(DataOutput out) throws IOException {
 
-			out.writeUTF ( root_ );
-			out.writeUTF ( state_.toString() );
-			//instead of: out.writeObject ( participants_.clone () );
-			if(participants_!=null){
-				out.writeBoolean(true);
-				out.writeInt(participants_.size());
+		out.writeUTF(root_);
+		out.writeUTF(state_.toString());
+		// instead of: out.writeObject ( participants_.clone () );
+		out.writeBoolean(heuristicCommit_);
+		out.writeLong(maxInquiries_);
 
-				for (Object participant : participants_) {
-					out.writeUTF(participant.getClass().getName());
-					((DataSerializable)participant).writeData(out);
-				}
-			} else {
-				out.writeBoolean(false);
+		out.writeUTF(stateHandler_.getState().toString());
+		// ((DataSerializable) stateHandler_).writeData(out);
+		// instead of: out.writeObject ( stateHandler_.clone () );
+
+		out.writeBoolean(activity_);
+		if (activity_) {
+			out.writeInt(localSiblingCount_);
+			out.writeBoolean(checkSiblings_);
+		}
+		out.writeBoolean(single_threaded_2pc_);
+
+		if (coordinator_ == null)
+			out.writeBoolean(false);
+		else {
+			out.writeBoolean(true);
+
+			// FIXME: TODO:
+			// out.writeObject ( coordinator_ );
+		}
+		if (participants_ != null) {
+			out.writeBoolean(true);
+			out.writeInt(participants_.size());
+
+			for (Object participant : participants_) {
+				out.writeUTF(participant.getClass().getName());
+				((DataSerializable) participant).writeData(out);
 			}
-				//
-				if ( coordinator_ == null )
-				    out.writeBoolean ( false );
-				else {
-				    out.writeBoolean ( true );
-				  //FIXME: TODO:
-				  //  out.writeObject ( coordinator_ );
-				}
-				out.writeBoolean ( heuristicCommit_ );
-				out.writeLong ( maxInquiries_ );
-				out.writeUTF(stateHandler_.getState().toString());
-	      // ((DataSerializable) stateHandler_).writeData(out);
-				//instead of: out.writeObject ( stateHandler_.clone () );
-				out.writeBoolean ( activity_ );
-				if ( activity_ ) {
-				    out.writeInt ( localSiblingCount_ );
-				    out.writeBoolean ( checkSiblings_ );
-				}
-				out.writeBoolean ( single_threaded_2pc_ );
-
-
-
-
-
+		} else {
+			out.writeBoolean(false);
+		}
 	}
 
 	public void readData(DataInput in) throws IOException {
 
-        try {
+		try {
 
 			root_ = (String) in.readUTF();
-			String str=in.readUTF();
-			state_ = TxState.valueOf(str);
-			boolean initialized=in.readBoolean();
-			if(initialized){
-				//intead of: participants_ = (Vector) in.readObject ();
-				int size=in.readInt();
-				for (int i = 0; i < size; i++) {
-					DataSerializable participant=(DataSerializable)ClassLoadingHelper.newInstance(in.readUTF());
-					participant.readData(in);
-				}
+			String state = in.readUTF();
+			state_ = TxState.valueOf(state);
+			heuristicCommit_ = in.readBoolean();
+			maxInquiries_ = in.readLong();
+			// instead of: stateHandler_ = (CoordinatorStateHandler) in.readObject ();
+
+			TxState txState = TxState.valueOf(in.readUTF());
+
+
+			activity_ = in.readBoolean();
+			if (activity_) {
+				localSiblingCount_ = in.readInt();
+				checkSiblings_ = in.readBoolean();
 			}
-				//
-				boolean readcoord = in.readBoolean ();
-				if ( readcoord ){
-					//Usefull when RecoveryCoordinator is CompositeCoordinator ????
-					//FIXME: TODO: coordinator_ = (RecoveryCoordinator) in.readObject ();
+			single_threaded_2pc_ = in.readBoolean();
+			boolean hasCoordinator = in.readBoolean();
+			if(hasCoordinator){
+				//TODO ?
+			}
+			if (TxState.ACTIVE.equals(txState)) {
+				stateHandler_ = new ActiveStateHandler((CoordinatorImp) coordinator_);
+			} else if (TxState.HEUR_ABORTED.equals(txState)) {
+				stateHandler_ = new HeurAbortedStateHandler((CoordinatorImp) coordinator_);
+			} else if (TxState.HEUR_COMMITTED.equals(txState)) {
+				stateHandler_ = new HeurCommittedStateHandler((CoordinatorImp) coordinator_);
+			} else if (TxState.HEUR_HAZARD.equals(txState)) {
+				stateHandler_ = new HeurHazardStateHandler((CoordinatorImp) coordinator_);
+			} else if (TxState.HEUR_MIXED.equals(txState)) {
+				stateHandler_ = new HeurMixedStateHandler((CoordinatorImp) coordinator_);
+			} else if (TxState.IN_DOUBT.equals(txState)) {
+				stateHandler_ = new IndoubtStateHandler((CoordinatorImp) coordinator_);
+			} else if (TxState.TERMINATED.equals(txState)) {
+				stateHandler_ = new TerminatedStateHandler((CoordinatorImp) coordinator_);
+			}
+			boolean initialized = in.readBoolean();
+			if (initialized) {
+				participants_=new Vector();
+				// intead of: participants_ = (Vector) in.readObject ();
+				int size = in.readInt();
+				for (int i = 0; i < size; i++) {
+					DataSerializable participant = (DataSerializable) ClassLoadingHelper.newInstance(in.readUTF());
+					participant.readData(in);
+					participants_.add(participant);
 				}
-
-				heuristicCommit_ = in.readBoolean ();
-				maxInquiries_ = in.readLong ();
-				//instead of: stateHandler_ = (CoordinatorStateHandler) in.readObject ();
-
-				TxState txState=TxState.valueOf(in.readUTF());
-				if (TxState.ACTIVE.equals(txState)){
-					stateHandler_=new ActiveStateHandler((CoordinatorImp)coordinator_);
-				} else
-					if (TxState.HEUR_ABORTED.equals(txState)){
-					stateHandler_=new HeurAbortedStateHandler((CoordinatorImp)coordinator_);
-				} else if (TxState.HEUR_COMMITTED.equals(txState)){
-					stateHandler_=new HeurCommittedStateHandler((CoordinatorImp)coordinator_);
-				} else if (TxState.HEUR_HAZARD.equals(txState)){
-					stateHandler_=new HeurHazardStateHandler((CoordinatorImp)coordinator_);
-				} else if (TxState.HEUR_MIXED.equals(txState)){
-					stateHandler_=new HeurMixedStateHandler((CoordinatorImp)coordinator_);
-				} else if (TxState.IN_DOUBT.equals(txState)){
-					stateHandler_=new IndoubtStateHandler((CoordinatorImp)coordinator_);
-				} else if (TxState.TERMINATED.equals(txState)){
-					stateHandler_=new TerminatedStateHandler((CoordinatorImp)coordinator_);
-				}
-
-				activity_ = in.readBoolean();
-				if ( activity_ ) {
-				    localSiblingCount_ = in.readInt();
-				    checkSiblings_ = in.readBoolean();
-				}
-				single_threaded_2pc_ = in.readBoolean();
-
+			}else{
+				participants_=new Vector();
+			}
 
 		} catch (InvalidClassException ex) {
 			// fix for 22174
-			throw (IOException) new IOException ( "Object of class " + ex.classname + " in transaction log" +
-					" not compatible with version found in classpath" ).initCause(ex);
+			throw (IOException) new IOException("Object of class " + ex.classname + " in transaction log" + " not compatible with version found in classpath").initCause(ex);
 		}
 
 	}
