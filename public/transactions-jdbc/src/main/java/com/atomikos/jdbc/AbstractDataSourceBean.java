9ffe00bac3bd438f22140a98aadf6a1f1788b37b
Guy Pardon <guy@atomikos.com>
2013-04-24 20:33:39 +0200

    BugzID: 104914 - deprecated testQuery for JDBC

--- public/transactions-jdbc/src/main/java/com/atomikos/jdbc/AbstractDataSourceBean.java
+++ public/transactions-jdbc/src/main/java/com/atomikos/jdbc/AbstractDataSourceBean.java
@@ -265,8 +265,8 @@ implements HeuristicDataSource, ConnectionPoolProperties, Referenceable, Seriali
 			throwAtomikosSQLException("Property 'minPoolSize' must be at least 0 and at most maxPoolSize, was: " + minPoolSize);
 		if ( getUniqueResourceName() == null )
 			throwAtomikosSQLException("Property 'uniqueResourceName' cannot be null");
-		if ( getTestQuery() == null ) 
-			LOGGER.logWarning ( this + ": no testQuery set - the connection pool will not be able to validate the connections!" );
+		if ( getTestQuery() != null ) 
+			LOGGER.logWarning ( this + ": testQuery set - this may cause performance problems if the database becomes permanently unreachable!" );
 		if ( getMinPoolSize() == DEFAULT_POOL_SIZE ) {
 			LOGGER.logWarning ( this + ": poolSize equals default - this may cause performance problems!" );
 		}
