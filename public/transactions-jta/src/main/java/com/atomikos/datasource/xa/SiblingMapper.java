bb8d36e93934f6b967d1f0a3e72916c207f3f058
pascalleclercq <none@none>
2013-05-16 13:24:11 +0200

    Organize Imports

--- public/transactions-jta/src/main/java/com/atomikos/datasource/xa/SiblingMapper.java
+++ public/transactions-jta/src/main/java/com/atomikos/datasource/xa/SiblingMapper.java
@@ -31,7 +31,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Stack;
 
 import com.atomikos.datasource.ResourceException;
 import com.atomikos.datasource.ResourceTransaction;
