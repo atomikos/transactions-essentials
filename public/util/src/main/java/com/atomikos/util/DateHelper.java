e40aa4b1a229914e724563868da4062cda8a65a5
Guy Pardon <guy@atomikos.com>
2013-10-17 19:32:30 +0200

    Extracted helper class for date formatting

---
+++ public/util/src/main/java/com/atomikos/util/DateHelper.java
@@ -0,0 +1,16 @@
+package com.atomikos.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateHelper {
+	
+	private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss:SSS");
+
+	public static String format(Date date) {
+		return dateFormatter.format(date);
+	}
+
+	
+	
+}
