d7ab32dcc6626eb9be9103393c150189080623fd
pascalleclercq <none@none>
2013-10-18 09:08:51 +0200

    replace SimpleDateFormat by commons lang's FastDateFormat as It is Thread safe and faster...

--- public/util/src/main/java/com/atomikos/util/DateHelper.java
+++ public/util/src/main/java/com/atomikos/util/DateHelper.java
@@ -1,11 +1,11 @@
 package com.atomikos.util;
 
-import java.text.SimpleDateFormat;
+import java.text.Format;
 import java.util.Date;
 
 public class DateHelper {
 	
-	private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss:SSS");
+	private static Format dateFormatter = FastDateFormat.getInstance("yyyy.MM.dd HH:mm:ss:SSS");
 
 	public static String format(Date date) {
 		return dateFormatter.format(date);
