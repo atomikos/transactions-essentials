5a62bac859d0ed240705e53a8cdfec71b3a5ea95
Guy Pardon <guy@atomikos.com>
2013-10-31 12:47:33 +0100

    Added JTA-specific init property names to  JtaTransactionServicePlugin

--- public/transactions-jta/src/main/java/com/atomikos/icatch/jta/JtaTransactionServicePlugin.java
+++ public/transactions-jta/src/main/java/com/atomikos/icatch/jta/JtaTransactionServicePlugin.java
@@ -5,6 +5,31 @@ import java.util.Properties;
 import com.atomikos.icatch.TransactionServicePlugin;
 
 public class JtaTransactionServicePlugin implements TransactionServicePlugin {
+	
+	/**
+	 * The name of the property that specifies the default timeout (in
+	 * milliseconds) that is set for transactions when no timeout is specified.
+	 *
+	 * Expands to {@value}.
+	 */
+	public static final String DEFAULT_JTA_TIMEOUT_PROPERTY_NAME = "com.atomikos.icatch.default_jta_timeout";
+
+	/**
+	 * The name of the property that indicates whether JTA transactions are to
+	 * be in serial mode or not.
+	 *
+	 * Expands to {@value}.
+	 */
+	public static final String SERIAL_JTA_TRANSACTIONS_PROPERTY_NAME = "com.atomikos.icatch.serial_jta_transactions";
+
+	/**
+	 * The name of the property indicating whether remote clients can start
+	 * transactions on this service or not.
+	 *
+	 * Expands to {@value}.
+	 */
+	public static final String CLIENT_DEMARCATION_PROPERTY_NAME = "com.atomikos.icatch.client_demarcation";
+
 
 	@Override
 	public void beforeInit(Properties properties) {
