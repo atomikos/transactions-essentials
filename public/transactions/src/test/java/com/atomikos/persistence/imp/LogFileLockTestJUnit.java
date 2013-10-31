70a3b257c8ea0875ada31d4b72fbea439985f173
Guy Pardon <guy@atomikos.com>
2013-10-31 09:25:33 +0100

    Added LogFileLock utility class

---
+++ public/transactions/src/test/java/com/atomikos/persistence/imp/LogFileLockTestJUnit.java
@@ -0,0 +1,43 @@
+package com.atomikos.persistence.imp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.atomikos.persistence.LogException;
+
+public class LogFileLockTestJUnit {
+
+	private LogFileLock lock;
+	
+	@Before
+	public void setUp() throws Exception {
+		lock = new LogFileLock("./", "LogFileLockTest");
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		if (lock != null) lock.releaseLock();
+	}
+
+	@Test
+	public void testLockWorksForFirstAcquisition() throws LogException {
+		lock.acquireLock();
+	}
+	
+	@Test(expected=LogException.class)
+	public void testLockFailsForSecondAcquisition() throws LogException {
+		lock.acquireLock();
+		lock.acquireLock();
+	}
+
+	@Test
+	public void testLockWorksAfterAcquisitionAndRelease() throws LogException {
+		lock.acquireLock();
+		lock.releaseLock();
+		lock.acquireLock();
+	}
+	
+}
