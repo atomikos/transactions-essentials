9209bd173325aca592b49868c36cd6a8d3597a0c
Guy Pardon <guy@atomikos.com>
2014-02-26 03:45:30 +0100

    BugzID: 115604 - indented code

--- public/transactions/src/main/java/com/atomikos/icatch/imp/TransactionStateHandler.java
+++ public/transactions/src/main/java/com/atomikos/icatch/imp/TransactionStateHandler.java
@@ -267,12 +267,12 @@ abstract class TransactionStateHandler implements SubTxAwareParticipant
         		setRollbackOnly();
         		// see case 115604
         		// transport the first exception here as return value
-            if (cause == null) {
-              cause = error;
-            } else {
-              // log the others which may still happen as error
-              LOGGER.logError("Unexpected error in beforeCompletion: ", error);
-            }       		
+        		if (cause == null) {
+        			cause = error;
+        		} else {
+        			// log the others which may still happen as error
+        			LOGGER.logError("Unexpected error in beforeCompletion: ", error);
+        		}       		
         	}
         	sync = localPopSynchronization();
         }
