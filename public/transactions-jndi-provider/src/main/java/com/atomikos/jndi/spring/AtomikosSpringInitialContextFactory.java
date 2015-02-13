043cbfbbbfeb91ee62398617cc9f0c821be231ce
pascalleclercq <none@none>
2015-02-13 09:58:38 +0100

    129645: Custom JNDI provider so Hibernate integration is seamless across Hibernate versions
    Task-Url: http://fogbugz.atomikos.com/default.asp?129645
    
    next step : code review !!!

--- public/transactions-jndi-provider/src/main/java/com/atomikos/jndi/spring/AtomikosSpringInitialContextFactory.java
+++ public/transactions-jndi-provider/src/main/java/com/atomikos/jndi/spring/AtomikosSpringInitialContextFactory.java
@@ -1,15 +1,15 @@
 package com.atomikos.jndi.spring;
 
-import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.naming.spi.InitialContextFactory;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceEditor;
@@ -17,54 +17,49 @@ import org.springframework.core.io.ResourceEditor;
 import com.atomikos.logging.Logger;
 import com.atomikos.logging.LoggerFactory;
 
-public class AtomikosSpringInitialContextFactory implements
-		InitialContextFactory {
+public class AtomikosSpringInitialContextFactory implements InitialContextFactory {
 
-	private static final Logger log =LoggerFactory.createLogger(AtomikosSpringInitialContextFactory.class);
-	private static Map<String,Object> cache = new HashMap<String,Object>();
+	private static final Logger log = LoggerFactory.createLogger(AtomikosSpringInitialContextFactory.class);
+	private static Map<String, Object> cache = new ConcurrentHashMap<String, Object>();
 
-    public Context getInitialContext(Hashtable environment) throws NamingException {
-        Resource resource = null;
-        Object value = environment.get(Context.PROVIDER_URL);
-        String key = "jndi.xml";
-        if (value == null) {
-            resource = new ClassPathResource(key);
-        }
-        else {
-            if (value instanceof Resource) {
-                resource = (Resource) value;
-            }
-            else {
-                ResourceEditor editor = new ResourceEditor();
-                key = value.toString();
-                editor.setAsText(key);
-                resource = (Resource) editor.getValue();
-            }
-        }
-        BeanFactory context = loadContext(resource, key);
-      //  Context answer = (Context) context.getBean("jndi");
-      //  if (answer == null) {
-      //      log.logWarning("No JNDI context available in JNDI resource: " + resource);
-        Context      answer = new DefaultContext(environment);
-      //  }
-        return answer;
-    }
+	public Context getInitialContext(Hashtable environment) throws NamingException {
+		Resource resource = null;
+		Object value = environment.get(Context.PROVIDER_URL);
+		String key = "atomikos.xml";
 
-    protected BeanFactory loadContext(Resource resource, String key) {
-        synchronized (cache) {
-            BeanFactory answer = (BeanFactory) cache.get(key);
-            if (answer == null) {
-                answer =  createContext(resource);
-                cache.put(key, answer);
-            }
-            return answer;
-        }
-    }
+		if (value == null) {
+			resource = new ClassPathResource(key);
+		} else {
+			if (value instanceof Resource) {
+				resource = (Resource) value;
+			} else {
+				ResourceEditor editor = new ResourceEditor();
+				key = value.toString();
+				editor.setAsText(key);
+				resource = (Resource) editor.getValue();
+			}
+		}
 
-    protected BeanFactory createContext(Resource resource) {
-        log.logInfo("Loading JNDI context from: " + resource);
-        return new XmlBeanFactory(resource);
-        //return new DefaultListableBeanFactory(resource);
-    }
+		BeanFactory context = loadContext(resource, key);
+		Context answer = new DefaultContext(environment);
+		return answer;
+	}
+
+	private BeanFactory loadContext(Resource resource, String key) {
+		synchronized (cache) {
+			BeanFactory answer = (BeanFactory) cache.get(key);
+			if (answer == null) {
+				answer = createContext(resource);
+				cache.put(key, answer);
+			}
+			return answer;
+		}
+	}
+
+	private BeanFactory createContext(Resource resource) {
+		log.logInfo("Loading JNDI context from: " + resource);
+		GenericXmlApplicationContext context = new GenericXmlApplicationContext(resource);
+		return context;
+	}
 
 }
