d2b87a98b26dcf188fde1e8dccb2ad660776e908
pascalleclercq <none@none>
2015-04-28 00:26:35 +0200

    remove dead code

--- public/transactions/src/test/java/com/atomikos/icatch/imp/SysExceptionTestJUnit.java
+++ public/transactions/src/test/java/com/atomikos/icatch/imp/SysExceptionTestJUnit.java
@@ -7,28 +7,8 @@ import com.atomikos.icatch.SysException;
 public class SysExceptionTestJUnit extends TestCase 
 {
 
-	private static void assertContains ( 
-			String msg , StackTraceElement[] st )
-	{
-		
-		boolean found = false;
-		if ( st == null || st.length == 0 ) fail ( "No elements");
-		for ( int i = 0 ; i < st.length ; i++ ) {
-			if ( st[i].toString().indexOf ( msg ) >= 0 ) found = true;
-		}
-		if ( ! found ) {
-			for ( int i = 0 ; i < st.length ; i++ ) {
-				System.out.println ( st[i] );
-				
-			}
-			System.out.println();
-			fail ( "Not found in stack trace: " + msg );
-		}
-	}
-	
 	public void testSysExceptionWithoutNestedErrors()
 	{
-		final String msg = "com.atomikos.icatch.SysException";
 		SysException se = new SysException ( null );
 		StackTraceElement[] st = se.getStackTrace();
 		assertNotNull ( st );
