224625b64f4706a2c2cf1c21c2e4350faf9ac4c6
pascalleclercq <none@none>
2015-09-20 21:28:56 +0200

    Fix some various junit tests : wasCommited was missing while creating coordinatorLogEntry.

--- public/transactions/src/main/java/com/atomikos/icatch/imp/CoordinatorImp.java
+++ public/transactions/src/main/java/com/atomikos/icatch/imp/CoordinatorImp.java
@@ -969,7 +969,7 @@ public class CoordinatorImp implements CompositeCoordinator, Participant,
     				i++;
     			}
 		
-    			CoordinatorLogEntry coordinatorLogEntry = new CoordinatorLogEntry(this.getCoordinatorId(), participantDetails);
+    			CoordinatorLogEntry coordinatorLogEntry = new CoordinatorLogEntry(this.getCoordinatorId(), this.isCommitted(), participantDetails);
     			return coordinatorLogEntry;
     		}	
     	}
