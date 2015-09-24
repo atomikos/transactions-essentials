b6d1b7e32fe9d3704dd1422cae333b8ca7939f15
Guy Pardon <guy@atomikos.com>
2015-09-24 11:48:31 +0200

    Cleaned tests for readibility

--- public/transactions-api/src/test/java/com/atomikos/recovery/CoordinatorLogEntryTestJUnit.java
+++ public/transactions-api/src/test/java/com/atomikos/recovery/CoordinatorLogEntryTestJUnit.java
@@ -10,66 +10,65 @@ public class CoordinatorLogEntryTestJUnit {
 
 	String tid = "TID";
 	
+	private CoordinatorLogEntry coordinatorLogEntry;
+
+	private TxState combined;
+	
+	 
+	private void givenCoordinatorLogEntryWithParticipantStates(TxState... states) {
+		coordinatorLogEntry = createCoordinatorLogEntryWithParticipantsInState(states);
+	}
+	
+	private void whenCombinedStateIsGotten() {
+		combined = coordinatorLogEntry.getCommitResult();
+	}
+	
+	private void thenCombinedStateIs(TxState expected) {
+		assertEquals(expected, combined);
+	}
+	
 	@Test
 	public void atLeastOneCommittingParticipantMeansCommitResultCommitting() {
-		//given
-		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryWithParticipantsInStare(TxState.HEUR_MIXED, TxState.COMMITTING);
-		//when
-		TxState combined = coordinatorLogEntry.getCommitResult();
-		
-		//Then
-		assertEquals(TxState.COMMITTING, combined);
+		givenCoordinatorLogEntryWithParticipantStates(TxState.HEUR_MIXED, TxState.COMMITTING);
+		whenCombinedStateIsGotten();
+		thenCombinedStateIs(TxState.COMMITTING);
 	}
 	
 	@Test
 	public void allParticipantsTerminatedMeansCommitResultTerminated() throws Exception {
-		//given
-		
-		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryWithParticipantsInStare(TxState.TERMINATED, TxState.TERMINATED);
-		//when
-		TxState combined = coordinatorLogEntry.getCommitResult();
-		
-		//Then
-		assertEquals(TxState.TERMINATED, combined);
+		givenCoordinatorLogEntryWithParticipantStates(TxState.TERMINATED, TxState.TERMINATED);
+		whenCombinedStateIsGotten();
+		thenCombinedStateIs(TxState.TERMINATED);
 	}
 	
 	
 	@Test
 	public void allParticipantsHeurHazardMeansCommitResultHeurHazard() throws Exception {
-		//given
-		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryWithParticipantsInStare(TxState.HEUR_HAZARD, TxState.HEUR_HAZARD);
-		//when
-		TxState combined = coordinatorLogEntry.getCommitResult();
-		//Then
-		assertEquals(TxState.HEUR_HAZARD, combined);
+		givenCoordinatorLogEntryWithParticipantStates(TxState.HEUR_HAZARD, TxState.HEUR_HAZARD);
+		whenCombinedStateIsGotten();
+		thenCombinedStateIs(TxState.HEUR_HAZARD);
 	}
 
 	@Test
 	public void allParticipantsHeurAbortedMeansCommitResultHeurAborted() throws Exception {
-		//given
-		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryWithParticipantsInStare(TxState.HEUR_ABORTED, TxState.HEUR_ABORTED);
-		//when
-		TxState combined = coordinatorLogEntry.getCommitResult();
-		//Then
-		assertEquals(TxState.HEUR_ABORTED, combined);
+		givenCoordinatorLogEntryWithParticipantStates(TxState.HEUR_ABORTED, TxState.HEUR_ABORTED);
+		whenCombinedStateIsGotten();
+		thenCombinedStateIs(TxState.HEUR_ABORTED);
 	}
 
 	
 	@Test
 	public void defaultMeansCommitResultHeurMixed() throws Exception {
 		
-		//given
-		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryWithParticipantsInStare(TxState.TERMINATED, TxState.HEUR_HAZARD,TxState.HEUR_ABORTED);
-		//when
-		TxState combined = coordinatorLogEntry.getCommitResult();
-		//Then
-		assertEquals(TxState.HEUR_MIXED, combined);
+		givenCoordinatorLogEntryWithParticipantStates(TxState.TERMINATED, TxState.HEUR_HAZARD,TxState.HEUR_ABORTED);
+		whenCombinedStateIsGotten();
+		thenCombinedStateIs(TxState.HEUR_MIXED);
 	}
 	
 	
 	
 
-	private CoordinatorLogEntry createCoordinatorLogEntryWithParticipantsInStare(TxState... states) {
+	private CoordinatorLogEntry createCoordinatorLogEntryWithParticipantsInState(TxState... states) {
 		ParticipantLogEntry[] participantDetails = new ParticipantLogEntry[states.length];
 		for (int i = 0; i < participantDetails.length; i++) {
 			participantDetails[i] = new ParticipantLogEntry(tid, "uri", 0,"description", states[i]);
