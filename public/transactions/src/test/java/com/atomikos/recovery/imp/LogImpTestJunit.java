4d70c5a686f41b930c08c4e4e5d66c5dfb372a85
pascalleclercq <none@none>
2015-10-09 14:53:11 +0200

    Completed LogImp test for OltpLog

--- public/transactions/src/test/java/com/atomikos/recovery/imp/LogImpTestJunit.java
+++ public/transactions/src/test/java/com/atomikos/recovery/imp/LogImpTestJunit.java
@@ -38,14 +38,64 @@ public class LogImpTestJunit {
 	}
 	
 	@Test
+	public void testTransitionFromInDoubtToCommittingIsAllowed() throws Exception {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.IN_DOUBT);
+		Mockito.when(logRepository.get(coordinatorLogEntry.coordinatorId)).thenReturn(coordinatorLogEntry);
+		thenWriteCommittingWorks();
+	}
+	
+	
+
+	@Test
 	public void testTransitionFromInDoubtToAbortingIsAllowed() throws Exception {
 		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.IN_DOUBT);
 		Mockito.when(logRepository.get(coordinatorLogEntry.coordinatorId)).thenReturn(coordinatorLogEntry);
 		thenWriteAbortingWorks();
 	}
 	
+	@Test
+	public void testTransitionFromNullToInDoubtIsAllowed() throws Exception {
+		thenWriteInDoubtWorks();
+	}
+	
+	@Test
+	public void testTransitionFromNullToCommittingIsAllowed() throws Exception {
+		thenWriteCommittingWorks();
+	}
+	
+
 	@Test(expected=IllegalStateException.class)
 	public void testAbortingWithoutPreviousWriteMustFail() throws Exception {
+		thenWriteAbortingFails();
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void testTransitionFromAbortingToInDoubtMustFail() throws Exception {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.ABORTING);
+		Mockito.when(logRepository.get(coordinatorLogEntry.coordinatorId)).thenReturn(coordinatorLogEntry);
+		thenWriteInDoubtFails();
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void testTransitionFromCommittingToInDoubtMustFail() throws Exception {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.ABORTING);
+		Mockito.when(logRepository.get(coordinatorLogEntry.coordinatorId)).thenReturn(coordinatorLogEntry);
+		thenWriteCommittingFails();
+	}
+	
+
+	@Test(expected=IllegalStateException.class)
+	public void testTransitionFromAbortingToCommittingMustFail() throws Exception {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.ABORTING);
+		Mockito.when(logRepository.get(coordinatorLogEntry.coordinatorId)).thenReturn(coordinatorLogEntry);
+		thenWriteCommittingFails();
+	}
+	
+	
+	@Test(expected=IllegalStateException.class)
+	public void testTransitionFromCommittingToAbortingMustFail() throws Exception {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.COMMITTING);
+		Mockito.when(logRepository.get(coordinatorLogEntry.coordinatorId)).thenReturn(coordinatorLogEntry);
 		thenWriteAbortingFails();
 	}
 	
@@ -53,22 +103,37 @@ public class LogImpTestJunit {
 	
 	
 	
+	private void thenWriteInDoubtFails() throws OltpLogException {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.IN_DOUBT);
+		whenWriteInOltp(coordinatorLogEntry);		
+	}
 	
 	
-	
-	
-	
-	
-	
+
+	private void thenWriteCommittingFails() throws OltpLogException {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.COMMITTING);
+		whenWriteInOltp(coordinatorLogEntry);
+		
+	}
+
 	private void thenWriteAbortingFails() throws OltpLogException {
 		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.ABORTING);
 		whenWriteInOltp(coordinatorLogEntry);
 		
 	}
-
+	private void thenWriteInDoubtWorks() throws OltpLogException {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.IN_DOUBT);
+		whenWriteInOltp(coordinatorLogEntry);
+		
+	}
 	private void thenWriteAbortingWorks() throws OltpLogException {
 		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.ABORTING);
 		whenWriteInOltp(coordinatorLogEntry);
+	}
+	private void thenWriteCommittingWorks() throws OltpLogException {
+		CoordinatorLogEntry coordinatorLogEntry = createCoordinatorLogEntryInState(TxState.COMMITTING);
+		whenWriteInOltp(coordinatorLogEntry);
+		
 	}
 
 	private void whenDeleteInOltp(String tid) throws OltpLogException {
