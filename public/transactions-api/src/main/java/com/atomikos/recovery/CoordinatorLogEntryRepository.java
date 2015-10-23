a4f16dc7dd5388a8df793ec0e8743bc963630cf9
pascalleclercq <none@none>
2015-10-23 22:25:12 +0200

    Added init(Properties) & close() methods (mainly for FileSystemCoordinatorLogEntryRepository lifecycle)

--- public/transactions-api/src/main/java/com/atomikos/recovery/CoordinatorLogEntryRepository.java
+++ public/transactions-api/src/main/java/com/atomikos/recovery/CoordinatorLogEntryRepository.java
@@ -1,17 +1,22 @@
 package com.atomikos.recovery;
 
 import java.util.Collection;
+import java.util.Properties;
 
 public interface CoordinatorLogEntryRepository {
 
+	
+	void init(Properties properties);
 	/**
 	 * @throws IllegalArgumentException If the same coordinatorLogEntry is already in the repository. 
 	 */
-	void put(String id,CoordinatorLogEntry coordinatorLogEntry) throws IllegalArgumentException;
+	void put(String id,CoordinatorLogEntry coordinatorLogEntry) throws IllegalArgumentException,LogWriteException;
 	
 	void remove(String id);
 
 	CoordinatorLogEntry get(String coordinatorId);
 
 	Collection<ParticipantLogEntry> findAllCommittingParticipants();
+	
+	void close();
 }
