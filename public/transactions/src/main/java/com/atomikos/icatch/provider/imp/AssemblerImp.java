5c4c45acd26e19f21d21cf45c5db0ad40591b27b
pascal.leclercq@gmail.com <pascal.leclercq@gmail.com>
2016-03-25 15:53:22 +0100

    Fixed added check registration message for unregistered users (by email

--- public/transactions/src/main/java/com/atomikos/icatch/provider/imp/AssemblerImp.java
+++ public/transactions/src/main/java/com/atomikos/icatch/provider/imp/AssemblerImp.java
@@ -99,7 +99,7 @@ public class AssemblerImp implements Assembler {
 		return configProperties;
 	}
 	private void checkRegistration(ConfigProperties configProperties) {
-		if (configProperties.getCompletedProperties().contains("com.atomikos.icatch.registered")) {
+		if (!configProperties.getCompletedProperties().contains("com.atomikos.icatch.registered")) {
 			String message ="Thanks for using Atomikos! Register at http://www.atomikos.com/Main/RegisterYourDownload to disable this message and receive FREE tips & advice.";
 			LOGGER.logWarning(message);
 			System.out.println(message);
