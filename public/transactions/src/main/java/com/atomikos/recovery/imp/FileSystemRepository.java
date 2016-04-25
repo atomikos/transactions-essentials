f416f0f7f8969a047a15188edea57df02a73e4ef
pascal.leclercq@gmail.com <pascal.leclercq@gmail.com>
2016-04-25 22:16:57 +0200

    Organized imports

--- public/transactions/src/main/java/com/atomikos/recovery/imp/FileSystemRepository.java
+++ public/transactions/src/main/java/com/atomikos/recovery/imp/FileSystemRepository.java
@@ -12,7 +12,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectStreamException;
 import java.io.StreamCorruptedException;
