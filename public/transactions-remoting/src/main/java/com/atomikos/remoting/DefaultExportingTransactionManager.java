ee591a7e407bc298ab93072d9b8caa7f6ed79d58
aubelm <martin.aubele.ext@msg.group>
2024-06-16 09:37:02 +0200

    https://github.com/atomikos/transactions-essentials/issues/217
    Error "Recursion detected"
    In ProcessA, Service A.a1(operation) remotecall Service B.b1 in
    ProcessB, and B.b1 remotecall ServiceA.a2 in ProcessA
    
    transaction failed with "recursive detect" exception when TM in ProcessA
    call prepare to itself

--- public/transactions-remoting/src/main/java/com/atomikos/remoting/DefaultExportingTransactionManager.java
+++ public/transactions-remoting/src/main/java/com/atomikos/remoting/DefaultExportingTransactionManager.java
@@ -68,6 +68,12 @@ public class DefaultExportingTransactionManager implements ExportingTransactionM
 		if (!ct.getTid().equals(extent.getParentTransactionId())) {
 		    throw new IllegalArgumentException("The supplied extent is for a different transaction: found " + extent.getParentTransactionId()+ " but expected " + ct.getTid());
 		}
+		for (Participant p : extent.getParticipants()) {
+			String rootId = ct.getCompositeCoordinator().getRootId();
+			// detect rccursive call
+			if (rootId != null && p.getURI().endsWith(rootId))
+				return;
+		}
 		ct.getExtent().add(extent);
 		Stack<Participant> participants = extent.getParticipants();
 		for (Participant p : participants) {
--- public/transactions-remoting/src/main/java/com/atomikos/remoting/twopc/ParticipantAdapter.java
+++ public/transactions-remoting/src/main/java/com/atomikos/remoting/twopc/ParticipantAdapter.java
@@ -73,6 +73,10 @@ public class ParticipantAdapter implements Participant {
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.logDebug("Calling prepare on " + getURI());
 		}
+		// happens for recursive calls
+		if (cascadeList.isEmpty()) {
+			return Participant.READ_ONLY;
+		}
 		try {
 			int result = target.request()
 					.buildPost(Entity.entity(cascadeList, HeaderNames.MimeType.APPLICATION_VND_ATOMIKOS_JSON))
